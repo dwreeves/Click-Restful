d5ce08f9655752eca46e4fef618d531491443aa5
dwreeves <xdanielreeves@gmail.com>
2020-11-07 20:04:54 -0500

    multicommand support

--- click_restful/core.py
+++ click_restful/core.py
@@ -78,7 +78,6 @@ class Logger:
             return self._old_echo(**kwargs)
 
 
-
 def _typecast(val, default):
     if default is not None:
         return type(default)(val)
@@ -118,30 +117,42 @@ def click_to_blueprint(
         **kwargs
 ) -> flask.Blueprint:
 
-    bp = flask.Blueprint(cmd.name, __name__, url_prefix=f'/{cmd.name}')
+    try:
+        with flask.current_app.app_context():
+            url_prefix = flask.current_app.extensions['click_restful'].url_prefix
+    except (RuntimeError, KeyError):
+        url_prefix = None
+    _add_prefix = lambda s: s if url_prefix is None else f'{url_prefix}/{s}'
+
+    bp = flask.Blueprint(cmd.name, __name__, url_prefix=_add_prefix(f'/{cmd.name}'))
     bp.cli.add_command(cmd)
 
-    def rest():
-        kwargs = {
-            i.name: i.default
-            for i in cmd.params
-        }
+    all_commands = getattr(cmd, 'commands', {'': cmd})
 
-        # Typecast
-        req_params = {
-            k: _typecast(v, kwargs[k])
-            for k, v in flask.request.args.items()
-        }
+    for _name, _cmd in all_commands.items():
 
-        kwargs.update(req_params)
+        def rest():
+            kwargs = {
+                i.name: i.default
+                for i in _cmd.params
+            }
 
-        with Logger() as log:
-            res = cmd.callback(**kwargs)
+            # Typecast
+            req_params = {
+                k: _typecast(v, kwargs[k])
+                for k, v in flask.request.args.items()
+            }
 
-        return _create_response(res=res, log=log, jsonify=jsonify)
+            kwargs.update(req_params)
 
-    rest.__doc__ = openapi_yaml(cmd)
-    bp.add_url_rule('/', 'rest', view_func=rest)
+            with Logger() as log:
+                res = _cmd.callback(**kwargs)
+
+            return _create_response(res=res, log=log, jsonify=jsonify)
+
+        rest.__doc__ = openapi_yaml(cmd)
+        rest.__name__ = _name
+        bp.add_url_rule(f'/{_name}', _name, view_func=rest)
 
     return bp
 
@@ -156,21 +167,24 @@ class ClickRestful(object):
     def __init__(
             self,
             app: flask.Flask = None,
+            url_prefix: str = None,
             config_prefix: str = 'CLICK_RESTFUL_'
     ):
+        _add_prefix = lambda s: s if url_prefix is None else f'{url_prefix}/{s}'
         self.app = None
         self.swagger = None
+        self.url_prefix = None
         self.swagger_config = {
             'headers': [],
             'specs': [{
-                'endpoint': 'swagger',
-                'route': '/click_restful.json',
+                'endpoint': 'click_restful_swagger',
+                'route': _add_prefix('/click_restful.json'),
                 'rule_filter': lambda rule: True,
                 'model_filter': lambda tag: True,
             }],
-            'static_url_path': '/flasgger_static',
+            'static_url_path': _add_prefix('/flasgger_static'),
             'swagger_ui': True,
-            'specs_route': '/apidocs/'
+            'specs_route': _add_prefix('/apidocs/')
         }
         self.template = {
             'info': {
@@ -197,11 +211,13 @@ class ClickRestful(object):
 
 def create_click_app(cmd=None, **kwargs):
     app = flask.Flask(__name__)
-    click_rest = ClickRestful()
-    click_rest.init_app(app)
-    if cmd:
-        bp = click_to_blueprint(cmd, **kwargs)
-        app.register_blueprint(bp)
+
+    with app.app_context():
+        click_rest = ClickRestful()
+        click_rest.init_app(app)
+        if cmd:
+            bp = click_to_blueprint(cmd, **kwargs)
+            app.register_blueprint(bp)
 
     @app.route('/')
     def index():
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,24 +2,69 @@ import pytest
 import click
 from .conftest import ClickCommandTestCase
 
+# =============================================================================
+
 
 @click.command()
+def hello():
+    click.echo('Hello World!')
+
+
+class TestHelloWorld(ClickCommandTestCase):
+    cmd = hello
+    routes = ['/hello/']
+    inputs = ['']
+    outputs = [None]
+    stdouts = ['Hello World!\n']
+
+
+# =============================================================================
+
+
+@click.command('hello')
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
-def hello(count, name):
+def hello2(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     count = min(count, 100)  # ADDED FOR SPEED
     for x in range(count):
         click.echo('Hello %s!' % name)
 
 
-class TestHelloWorld(ClickCommandTestCase):
-    cmd = hello
-    routes = ['/hello/?count=3&name=Bob']
+class TestHelloWorld2(ClickCommandTestCase):
+    cmd = hello2
+    routes = ['/hello?count=3&name=Bob']
     inputs = ['--count 3 --name Bob']
     outputs = [None]
     stdouts = ['Hello Bob!\nHello Bob!\nHello Bob!\n']
 
 
-command_specs = [TestHelloWorld]
+# =============================================================================
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def initdb():
+    click.echo('Initialized the database')
+
+@click.command()
+def dropdb():
+    click.echo('Dropped the database')
+
+cli.add_command(initdb)
+cli.add_command(dropdb)
+
+
+class TestBasicGroup(ClickCommandTestCase):
+    cmd = cli
+    routes = ['/cli/initdb', '/cli/dropdb']
+    inputs = ['initdb', 'dropdb']
+    outputs = [None, None]
+    stdouts = ['Initialized the database\n', 'Dropped the database\n']
+
+
+command_specs = [TestHelloWorld2]
